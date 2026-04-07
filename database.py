2993c3ace5bb1119bd84c4f0cfeec727c7cea8a1
andrearce0 <andrearce343@gmail.com>
2026-04-07 13:17:01 -0300

    Database refatorado

--- database.py
+++ database.py
@@ -1,12 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./restaurante.db"
+load_dotenv()
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False} 
-)
+DB = os.getenv("DATABASE_URL")
+
+engine = create_engine(DB)
 
 SessionLocal = sessionmaker(autocommit = False,
 autoflush=False, bind=engine)
